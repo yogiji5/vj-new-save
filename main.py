a0b499734c09bee0d4e8813e3559ddfc22019801
yogiji5 <gimopo.l562@gmail.com>
2024-10-21 12:51:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import threading
 import json
 from os import environ
 
-bot_token = environ.get("TOKEN", "") 
-api_hash = environ.get("HASH", "") 
-api_id = int(environ.get("ID", ""))
+bot_token = environ.get("TOKEN", "7478653130:AAHwQ07X0ei66I-6b6lBfC9WiFZ-c-iYv5c") 
+api_hash = environ.get("HASH", "b3611c291bf82d917637d61e4a136535") 
+api_id = int(environ.get("ID", "23080322"))
 bot = Client("mybot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-ss = environ.get("STRING", "")
+ss = environ.get("STRING", "BAFgLYIAPwR4P_tICKd2uf_etLU02a2_hu8bWsFaMRjqM9rtNVj28wUUCXdSWC7bRUpQZhcwf0BULgg7LIjiZpHGJXCXqA9EUAVl02DZavQhWqJaB2IilNdgqIwsXOw1huHuL3dUU2vqf4vDXzLvOrTZvbvfi5gJk9oLd3f7ycbm2zK0CRHpvkTzN_iWCqnjgbZRWSI6L9OG4R6qeKM0K_QUyAcQlP2SwI7vEwHgCdIwhgh_3_rmUdESx-2fpj5-4heI8hnNw4nDpUGXrBSdHrqkMBCBh1aVQEdVWrAyJ-e_HyCn1Twe-4El3RQ-sh7rFxIfSM5G8zZEKDph_DXTkstu1JFB7QAAAAGCggAWAA")
 if ss is not None:
 	acc = Client("myacc" ,api_id=api_id, api_hash=api_hash, session_string=ss)
 	acc.start()
